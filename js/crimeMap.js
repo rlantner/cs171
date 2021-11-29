324580e0d9b5c321438a1ac899e26a94737483bc
juliakennish11 <43094009+juliakennish11@users.noreply.github.com>
2021-11-29 16:23:34 -0500

    Update crimeMap.js

--- js/crimeMap.js
+++ js/crimeMap.js
@@ -24,10 +24,6 @@ class CrimeMap {
 
         vis.map = L.map(vis.parentElement).setView(vis.mapCenter, vis.zoomFactor);
 
-        // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        // 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        // }).addTo(vis.map);
-
         L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
             maxZoom: 20,
             attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
@@ -57,16 +53,15 @@ class CrimeMap {
                 d.OBJECTID = +d.OBJECTID
             }
         )
-
-        vis.filteredCrime = vis.peopleCrime.filter(d => (d["Crime Category"] === "injury/homicide")
-                && (d.YEAR === 2018)
+        vis.filteredCrime = vis.peopleCrime.filter(d => (d.SHOOTING === 1 || d.OFFENSE_CODE_GROUP === 'Homicide')
+                && (d.YEAR === 2018 || d.YEAR === 2017 || d.YEAR === 2016)
                 && (d.HOUR < 6 || d.HOUR > 18))
         if (vis.zoomBool === "True") {
-            vis.filteredLight = vis.lightData.filter(d => d.OBJECTID%2 === 1 && d.Lat < 42.34 && d.Lat > 42.32 && d.Long < -71.065 && d.Long > -71.085)
+            vis.filteredLight = vis.lightData.filter(d => d.OBJECTID%2 === 1 && d.Lat < 42.335 && d.Lat > 42.315 && d.Long < -71.075 && d.Long > -71.095)
         }
 
-        console.log(vis.filteredCrime)
-        console.log(vis.filteredLight)
+        // console.log(vis.filteredCrime)
+        // console.log(vis.filteredLight)
 
         // Update the visualization
         vis.updateVis();
@@ -87,7 +82,7 @@ class CrimeMap {
         if (lights === 1) {
                 if (vis.zoomBool === "True") {
                     vis.filteredLight.forEach((d, i) => {
-                        console.log("light light baby")
+                        // console.log("light light baby")
                         lights = L.marker([d.Lat, d.Long], { icon: lightMarker })
                             .addTo(vis.map)
                     })
@@ -99,6 +94,12 @@ class CrimeMap {
             let popupContent =  "<strong>" + d.OFFENSE_DESCRIPTION + "</strong><br/>";
             popupContent += "Date: " + d.OCCURRED_ON_DATE + "<br/>";
             popupContent += "Street: " + d.STREET + "<br/>";
+            if (d.SHOOTING === 1) {
+                popupContent += "Shooting: Yes<br/>";
+            }
+            else{
+                popupContent += "Shooting: No<br/>";
+            }
             popupContent += "Latitude: " + d.LAT + "<br/>";
             popupContent += "Longitude: " + d.LONG + "<br/>";
             L.marker([d.LAT, d.LONG], { icon: crimeMarker })
